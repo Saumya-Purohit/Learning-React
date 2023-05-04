2586fb9e779e1fc4340f2f3f3d5c0a002241c2cf
Saumya-Purohit <saumyanotes@gmail.com>
2023-05-04 16:26:07 +0530

    learning react

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -3,18 +3,8 @@ import './App.css';
 let name="Saumya"
 function App() {
   return (
-    <>
-    <nav>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </nav>
-    <div className="container">
-      <h1>Hello {name} !</h1>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus possimus harum porro quidem soluta perferendis maiores id placeat excepturi cum tempore officiis quasi, molestias itaque nostrum inventore distinctio atque nam.</p>
-    </div>
-    </>
-
+  <>
+  </>
   );
 }
 
